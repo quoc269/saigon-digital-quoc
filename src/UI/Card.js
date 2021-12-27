df9bda86f046a667870bd3dbbefcc3da64b5503d
quoc269 <quoc269@gmail.com>
2021-12-27 15:37:34 +0700

    update card

--- src/UI/Card.js
+++ src/UI/Card.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import classes from './Card.module.css';
+import 'bootstrap/dist/css/bootstrap.css';
 const Card = ({ children }) => {
-	return <div className={classes.out} >
-				<div className={classes.card}>
-					<div className={classes.content}>{children}</div>
-				</div>
-			</div>;
+	return (
+		<div className={`${classes.out} py-5 px-5 `}>			
+				<div className={`${classes.card}`}>
+					<div className={`${classes.content} p-5`}>{children}</div>
+				</div>			
+		</div>
+		)
 };
 
 export default Card;
