f46241d8c91bf95e81ad83fcb884b3840cc144d9
quoc269 <quoc269@gmail.com>
2021-12-27 17:05:19 +0700

    update responsive card

--- src/UI/Card.js
+++ src/UI/Card.js
@@ -3,9 +3,9 @@ import classes from './Card.module.css';
 import 'bootstrap/dist/css/bootstrap.css';
 const Card = ({ children }) => {
 	return (
-		<div className={`${classes.out} py-5 px-5 `}>			
+		<div className={`${classes.out}`}>			
 				<div className={`${classes.card}`}>
-					<div className={`${classes.content} p-5`}>{children}</div>
+					<div className={`${classes.content}`}>{children}</div>
 				</div>			
 		</div>
 		)
